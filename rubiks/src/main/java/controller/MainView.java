88d09d9bb37160f77a0ac9e9d7053a929a21c2b2
Javier Torres <javierandrestorresreyes@gmail.com>
2018-12-09 00:16:09 -0500

    Visualizacion del cubo en 2D

--- rubiks/src/main/java/controller/MainView.java
+++ rubiks/src/main/java/controller/MainView.java
@@ -8,136 +8,181 @@ import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
+import model.Cube;
+import model.Piece;
 
 public class MainView {
 
-    @FXML
-    private ResourceBundle resources;
+	@FXML
+	private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+	@FXML
+	private URL location;
 
-    @FXML
-    private Canvas canvas;
+	@FXML
+	private Canvas canvas;
 
-    @FXML
-    private JFXButton btnF;
+	@FXML
+	private JFXButton btnF;
 
-    @FXML
-    private JFXButton btnFPrime;
+	@FXML
+	private JFXButton btnFPrime;
 
-    @FXML
-    private JFXButton btnR;
+	@FXML
+	private JFXButton btnR;
 
-    @FXML
-    private JFXButton btnRPrime;
+	@FXML
+	private JFXButton btnRPrime;
 
-    @FXML
-    private JFXButton btnU;
+	@FXML
+	private JFXButton btnU;
 
-    @FXML
-    private JFXButton btnUPrime;
+	@FXML
+	private JFXButton btnUPrime;
 
-    @FXML
-    private JFXButton btnL;
+	@FXML
+	private JFXButton btnL;
 
-    @FXML
-    private JFXButton btnLPrime;
+	@FXML
+	private JFXButton btnLPrime;
 
-    @FXML
-    private JFXButton btnB;
+	@FXML
+	private JFXButton btnB;
 
-    @FXML
-    private JFXButton btnBPrime;
+	@FXML
+	private JFXButton btnBPrime;
 
-    @FXML
-    private JFXButton btnD;
+	@FXML
+	private JFXButton btnD;
 
-    @FXML
-    private JFXButton btnDPrime;
+	@FXML
+	private JFXButton btnDPrime;
 
-    @FXML
-    private JFXButton btnShuffle;
+	@FXML
+	private JFXButton btnShuffle;
 
-    @FXML
-    void b(ActionEvent event) {
+	private Cube cube;
 
-    }
+	@FXML
+	void b(ActionEvent event) {
 
-    @FXML
-    void bPrime(ActionEvent event) {
+	}
 
-    }
+	@FXML
+	void bPrime(ActionEvent event) {
 
-    @FXML
-    void d(ActionEvent event) {
+	}
 
-    }
+	@FXML
+	void d(ActionEvent event) {
 
-    @FXML
-    void dPrime(ActionEvent event) {
+	}
 
-    }
+	@FXML
+	void dPrime(ActionEvent event) {
 
-    @FXML
-    void f(ActionEvent event) {
+	}
 
-    }
+	@FXML
+	void f(ActionEvent event) {
 
-    @FXML
-    void fPrime(ActionEvent event) {
+	}
 
-    }
+	@FXML
+	void fPrime(ActionEvent event) {
 
-    @FXML
-    void l(ActionEvent event) {
+	}
 
-    }
+	@FXML
+	void l(ActionEvent event) {
 
-    @FXML
-    void lPrime(ActionEvent event) {
+	}
 
-    }
+	@FXML
+	void lPrime(ActionEvent event) {
 
-    @FXML
-    void r(ActionEvent event) {
+	}
 
-    }
+	@FXML
+	void r(ActionEvent event) {
 
-    @FXML
-    void rPrime(ActionEvent event) {
+	}
 
-    }
+	@FXML
+	void rPrime(ActionEvent event) {
 
-    @FXML
-    void shuffle(ActionEvent event) {
+	}
 
-    }
+	@FXML
+	void shuffle(ActionEvent event) {
 
-    @FXML
-    void u(ActionEvent event) {
+	}
 
-    }
+	@FXML
+	void u(ActionEvent event) {
 
-    @FXML
-    void uPrime(ActionEvent event) {
-    	canvas.getGraphicsContext2D().fillOval(10, 60, 30, 30);;
-    }
+	}
 
-    @FXML
-    void initialize() {
-    	drawCube();
+	@FXML
+	void uPrime(ActionEvent event) {
 
-    }
+	}
+
+	@FXML
+	void initialize() {
+		this.cube = new Cube();
+		drawCube();
+
+	}
 
 	private void drawCube() {
 		GraphicsContext gc = canvas.getGraphicsContext2D();
-		gc.setFill(Color.GREEN);
-        gc.setStroke(Color.BLUE);
-        gc.setLineWidth(5);
-        gc.strokeLine(40, 10, 10, 40);
-        
-		
+		gc.setStroke(Color.BLACK);
+
+		drawFace(gc, Cube.F, 95, 95);
+		drawFace(gc, Cube.R, 185, 95);
+		drawFace(gc, Cube.U, 95, 5);
+		drawFace(gc, Cube.L, 5, 95);
+		drawFace(gc, Cube.B, 275, 95);
+		drawFace(gc, Cube.D, 95, 185);
+
+	}
+
+	private void drawFace(GraphicsContext gc, int face, int x, int y) {
+
+		gc.setLineWidth(2);
+		int[][] info = cube.getFace(face);
+		for (int i = 0; i < info.length; i++) {
+			for (int j = 0; j < info.length; j++) {
+				switch (info[i][j]) {
+				case Piece.GREEN:
+					gc.setFill(Color.GREEN);
+					break;
+				case Piece.RED:
+					gc.setFill(Color.RED);
+					break;
+				case Piece.WHITE:
+					gc.setFill(Color.WHITE);
+					break;
+				case Piece.ORANGE:
+					gc.setFill(Color.DARKORANGE);
+					break;
+				case Piece.BLUE:
+					gc.setFill(Color.BLUE);
+					break;
+				case Piece.YELLOW:
+					gc.setFill(Color.YELLOW);
+					break;
+				}
+				gc.fillRect(x, y, 30, 30);
+				gc.strokeRect(x, y, 30, 30);
+				x += 30;
+			}
+			x -= 90;
+			y += 30;
+		}
+		y -= 90;
+		gc.setLineWidth(3);
+		gc.strokeRect(x, y, 90, 90);
 	}
 }
--- rubiks/src/main/java/model/Cube.java
+++ rubiks/src/main/java/model/Cube.java
@@ -1,9 +1,16 @@
 package model;
 
 public class Cube {
-	
+
+	public static final int F = 0;
+	public static final int R = 1;
+	public static final int U = 2;
+	public static final int L = 3;
+	public static final int B = 4;
+	public static final int D = 5;
+
 	private Piece[][][] cube;
-	
+
 	public Cube() {
 		cube = new Piece[3][3][3];
 		for (int i = 0; i < cube.length; i++) {
@@ -13,6 +20,54 @@ public class Cube {
 				}
 			}
 		}
+	}
+
+	public int[][] getFace(int face) {
+		int[][] result = new int[3][3];
+		switch (face) {
+		case F:
+			for (int i = 0; i < result.length; i++) {
+				for (int j = 0; j < result.length; j++) {
+					result[i][j] = cube[i][j][0].getF();
+				}
+			}
+			break;
+		case R:
+			for (int i = 0; i < result.length; i++) {
+				for (int j = 0; j < result.length; j++) {
+					result[i][j] = cube[i][2][j].getR();
+				}
+			}
+			break;
+		case U:
+			for (int i = 0; i < result.length; i++) {
+				for (int j = 0; j < result.length; j++) {
+					result[i][j] = cube[0][j][2-i].getU();
+				}
+			}
+			break;
+		case L:
+			for (int i = 0; i < result.length; i++) {
+				for (int j = 0; j < result.length; j++) {
+					result[i][j] = cube[i][0][2-j].getL();
+				}
+			}
+			break;
+		case B:for (int i = 0; i < result.length; i++) {
+			for (int j = 0; j < result.length; j++) {
+				result[i][j] = cube[i][2-j][2].getB();
+			}
+		}
+		break;
+		case D:
+			for (int i = 0; i < result.length; i++) {
+				for (int j = 0; j < result.length; j++) {
+					result[i][j] = cube[2][j][i].getD();
+				}
+			}
+			break;
+		}
+		return result;
 	}
 
 }
